af2e11a44414fbaa2cb139a3131cd75d2b92f3f1
Vinicius Ishizaki <vinicius.ishizaki@gmail.com>
2018-11-05 16:12:20 -0500

    correted if statement

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,8 +8,8 @@ class GamesController < ApplicationController
     dictionary_serialized = open(url).read
     dictionary_api = JSON.parse(dictionary_serialized)
     @found_the_word = dictionary_api["found"] #will return true or false
-    parsed_word = dictionary_api["word"] #will return the word
-    parsed_length = dictionary_api["length"] #will return the length
+    # parsed_word = dictionary_api["word"] #will return the word
+    # parsed_length = dictionary_api["length"] #will return the length
   end
 
   def new
@@ -24,14 +24,15 @@ class GamesController < ApplicationController
 
     searchWord = searchWord(@user_word)
 
-    if @user_word_split.all? {|letter| @user_word_split.count(letter) <= @new_array.count(letter)}
-      if @found_the_word == true
+
+    if @found_the_word == true
+      if @user_word_split.all? {|letter| @user_word_split.count(letter) <= @new_array.count(letter)}
         return @word_score = "Congratulations, #{@user_word} is a valid english word"
       else
-        return @word_score = "Sorry but #{@user_word} does not seem to be a valid english word"
+        return @word_score = "Sorry but #{@user_word} can't be built out of #{@new_array}."
       end
     else
-      return @word_score = "Sorry but #{@user_word} can't be built out of #{@new_array}."
+      return @word_score = "Sorry but #{@user_word} does not seem to be a valid english word"
     end
   end
 end
